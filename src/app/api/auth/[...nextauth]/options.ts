715e719a4580da1cd1f5066bac4537e7edd1fe64
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-08 19:20:31 +0530

    Feat: successfully added google provider and login

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,47 @@
 import NextAuth from "next-auth"
 import { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/server/db";
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
 
 export const authOptions:NextAuthOptions = {
-  // Configure one or more authentication providers
+    session:{
+        strategy: "jwt",
+    },
   providers: [
     // ...add more providers here
+    GoogleProvider({
+        clientId: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        authorization: {
+            params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+            }
+          }
+      })
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if(!profile?.email){
+        throw new Error("No email returned from Google")
+      }
+      await prisma.user.upsert({
+        where: { email: profile.email },
+        create: {
+          email: profile.email,
+          name: profile.name,
+        },
+        update: {
+            name: profile.name,
+        },
+      });
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  }
 }
 
 export default NextAuth(authOptions)
