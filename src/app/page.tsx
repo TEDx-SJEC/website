9fd28426a0fc3a5227c817cc6faf25c55454c5bd
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-08 22:16:10 +0530

    Add user table to database schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 import { SessionProvider } from "next-auth/react"
+import prisma from "@/server/db";
 
-export default function Home() {
-  
+export default function Home() { 
+  prisma.user.create({
+    data: {
+      email: "joywin@",
+      name: "Joywin",
+    }});
+
+
+  prisma.user.findMany().then(console.log)
   return (
     <SessionProvider>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
