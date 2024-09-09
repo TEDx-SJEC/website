5044f12fd66a8d91d8de56759b898e5d7701d4fa
vyshnav <vyshnavravi599@gmail.com>
2024-09-09 09:13:16 +0530

    Refactor: Remove unnecessary code and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-"use client";
-import { SessionProvider } from "next-auth/react"
-import prisma from "@/server/db";
-
-export default function Home() { 
-  prisma.user.create({
-    data: {
-      email: "joywin@",
-      name: "Joywin",
-    }});
-
-
-  prisma.user.findMany().then(console.log)
-  return (
-    <SessionProvider>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
-    </SessionProvider>
-  );
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="bg-white">
+              
+            </div>
+        </main>
+    );
 }
