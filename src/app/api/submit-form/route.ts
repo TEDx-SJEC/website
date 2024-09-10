9a2ba3993f00441732c521beae2481867f1387a4
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-10 16:21:24 +0530

    Added zod validation for reg form

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
+import { RegistrationFormSchema, TRegistrationForm } from "@/utils/zod-schemas";
 
 export async function GET() {
   return NextResponse.json({ message: "Hello from the API!" });
@@ -7,21 +8,31 @@ export async function GET() {
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const body = req.body;
+    const body: TRegistrationForm = req.body;
     console.log(body);
+    const validationResult = RegistrationFormSchema.safeParse(body);
+
+    if (!validationResult.success) {
+      throw new Error(
+        "Validation error: " +
+          validationResult.error.errors.map((e) => e.message).join(", ")
+      );
+    }
 
     const {
       name,
       usn,
       email,
       contact,
+      designation,
       photo_url,
       referral_id,
       college_id_card,
-      college,
+      entity_name,
+      created_by_id,
     } = body;
 
-    return NextResponse.json({ message: "Hello from the API!" });
+    return NextResponse.json({ message: body });
   } catch (error) {
     console.error(error);
     res;
