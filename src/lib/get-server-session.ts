06b55c2d58c58d4092f9c36820547e4a4415b854
vyshnav <vyshnavravi599@gmail.com>
2024-09-11 17:15:55 +0530

    Refactor: Update import path for auth options in get-server-session.ts

--- src/lib/get-server-session.ts
+++ src/lib/get-server-session.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { authOptions } from "./auth-options";
 
 export async function getServerSideSession() {
     return await getServerSession(authOptions);
