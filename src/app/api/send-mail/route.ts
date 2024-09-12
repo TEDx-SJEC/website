4c16376c8af872a13968f079486275e2f33a873d
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-12 23:19:20 +0530

    Refactor: Add OTP generator for email verification

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,17 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import sendEmail from "@/utils/sendMail";
+import otpGenerator from "otp-generator";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
+  const otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  });
+
   console.log(body);
   const mailResponse = await sendEmail({
     email: body.email,
     name: body.name,
-    OTP: `http://localhost:3000/verify-email?token=${body.token}`,
+    OTP: otp,
   });
 
-  return NextResponse.json({ message: "Email sent successfully!",mailResponse });
-  
+  return NextResponse.json({
+    message: "Email sent successfully!",
+    mailResponse,
+  });
 }
 
 export async function GET() {
