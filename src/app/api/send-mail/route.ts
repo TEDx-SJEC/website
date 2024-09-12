08ee61786307579c75cbf646e252bd3afe343558
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-12 23:37:39 +0530

    feat: save verification otp request in database

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import sendEmail from "@/utils/sendMail";
 import otpGenerator from "otp-generator";
+import prisma from "@/server/db";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -8,6 +9,17 @@ export async function POST(req: NextRequest) {
     upperCaseAlphabets: false,
     lowerCaseAlphabets: false,
     specialChars: false,
+  });
+
+  const expiresIn = 10; // 10 minutes
+  const expiresAt = new Date(Date.now() + expiresIn * 60 * 1000);
+
+  await prisma.verificationRequest.create({
+    data: {
+      identifier: body.email,
+      otp,
+      expires: expiresAt,
+    },
   });
 
   console.log(body);
