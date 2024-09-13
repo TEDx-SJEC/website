8be0dc1c7377b5747fe0b0332df6f0b5b5af15e8
vyshnav <vyshnavravi599@gmail.com>
2024-09-13 21:13:38 +0530

    Feat: integrate React Query with session provider for improved data fetching

--- src/components/Layout/Provider.tsx
+++ src/components/Layout/Provider.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export default function Providers({ children }: { children: ReactNode }) {
+    const queryClient = new QueryClient();
     return (
         <>
-            <SessionProvider>{children}</SessionProvider>
+            <SessionProvider>
+                <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+            </SessionProvider>
         </>
     );
 }
