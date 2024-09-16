1b6d1f39ccf40d9f2c9083f442c4408c3b403ae3
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-16 10:25:13 +0530

    Added email template

---
+++ src/components/email-template.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+interface EmailTemplateProps {
+  name: string;
+  email: string;
+  OTP: string;
+}
+
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  name,
+  email,
+  OTP,
+}) => (
+  <div>
+    <h1>Hello ${name},</h1>
+    <p>Thank you for registering for Tedx 2024.</p>
+    <p>Your One-Time Password (OTP) for email verification is:</p>
+    <h2 style={{ color: "#f44336" }}>{OTP}</h2>
+    <p>
+      Please enter this OTP to complete your registration. The OTP is valid for
+      10 minutes.
+    </p>
+    <p>Thank you!</p>
+    <p>
+      <strong>Tedx SJEC Team</strong>
+    </p>
+  </div>
+);
