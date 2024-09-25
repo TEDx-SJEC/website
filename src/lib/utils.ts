8917b3f616fb7e3feaa90d72cc5977a6a6c798a0
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-25 11:03:35 +0530

    fix: remove auth() function in utils which was causing the "url undefined aka env undefined"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export async function auth() {
-  const session = await getServerSideSession();
-  if (session && session.user) {
-    return session.user;
-  } else return null;
-}
+
