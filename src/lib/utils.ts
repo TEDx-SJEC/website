b183c23173dd487e3f486434a1c8a67dba8e0bae
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-09-25 10:02:34 +0530

    added auth() to get user from session

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { getServerSideSession } from "./get-server-session";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function auth() {
+  const session = await getServerSideSession();
+  if (session && session.user) {
+    return session.user;
+  } else return null;
 }
