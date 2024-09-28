3186222657dc98a1be3d6f79180c22341f7ab58c
vyshnav <vyshnavravi599@gmail.com>
2024-09-28 16:13:02 +0530

    Refactor middleware to include admin role check

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,17 @@ import { getToken } from "next-auth/jwt";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const url = request.nextUrl;
+    const token = await getToken({ req: request });
+    const url = request.nextUrl;
 
-  if (url.pathname === "/admin") {
-    if (token?.role!=="ADMIN") {
-      return NextResponse.redirect(new URL("/", request.url));
+    if (url.pathname.includes("/admin")) {
+        if (token?.role !== "ADMIN") {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
     }
-  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/:path*", "/"],
+    matcher: ["/:path*", "/"],
 };
