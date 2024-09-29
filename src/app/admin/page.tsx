604386c57720bb4914e0ea9725fdf20fd917d5fe
vyshnav <vyshnavravi599@gmail.com>
2024-09-29 19:21:29 +0530

    Refactor admin page component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 import { Coupon } from "@/components/Admin/code-generation-card";
 import { useSession } from "next-auth/react";
+import React from "react";
 export default function AdminPage() {
-  const { data: session } = useSession();
+    const { data: session } = useSession();
 
-  if (!session || session.user.role != "ADMIN") {
-    return <div>Unauthorized </div>;
-  }
+    if (!session || session.user.role != "ADMIN") {
+        return <div>Unauthorized </div>;
+    }
 
-  return (
-    <>
-      <div className="w-screen h-screen flex justify-center items-center">
-        <Coupon session={session} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="w-screen h-screen flex justify-center items-center">
+                <Coupon session={session} />
+            </div>
+        </>
+    );
 }
