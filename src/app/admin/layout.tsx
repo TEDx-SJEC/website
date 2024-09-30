1df0f9c59f87338114b91111f94969e3935831f7
vyshnav <vyshnavravi599@gmail.com>
2024-09-30 07:21:22 +0530

    Add session validation and authorization checks in admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Layout/Provider";
 import { AdminNavbar } from "@/components/Admin/Navbar/navbar";
+import { useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,16 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const { data: session } = useSession({
+        required: true,
+    });
+    if (!session) {
+        return <div>Unauthorized</div>;
+    }
+
+    if (session.user.role !== "ADMIN") {
+        return <div>Forbidden</div>;
+    }
     return (
         <html lang="en">
             <body className={inter.className}>
