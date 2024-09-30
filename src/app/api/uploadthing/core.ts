0253f0a9e4b8c540afa634f818d3a71c6a2bff24
vyshnav <vyshnavravi599@gmail.com>
2024-09-30 07:22:38 +0530

    Refactor uploadthing core

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,34 +1,33 @@
-import { getServerSideSession } from "@/lib/get-server-session";
+import { getServerSession } from "next-auth";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
 const auth = async (req: Request) => {
-  const session = await getServerSideSession();
-  if (session && session.user) {
-    return session.user;
-  } else return null;
+    const session = await getServerSession();
+
+    return session?.user;
 };
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
-      console.log("Middleware for imageUploader", req.url);
-      const user = await auth(req);
+    imageUploader: f({ image: { maxFileSize: "4MB" } })
+        .middleware(async ({ req }) => {
+            console.log("Middleware for imageUploader", req.url);
+            const user = await auth(req);
 
-      if (!user) throw new UploadThingError("Unauthorized");
+            if (!user) throw new UploadThingError("Unauthorized");
 
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // console.log("Upload complete for userId:", metadata.userId);
+            return { userId: user.id };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            // console.log("Upload complete for userId:", metadata.userId);
 
-      // console.log("file url", file.url);
+            // console.log("file url", file.url);
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
+            // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+            return { uploadedBy: metadata.userId };
+        }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
