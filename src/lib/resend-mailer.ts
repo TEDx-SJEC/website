16055139620dcfcfe9ef16a5308daca0c3328ddc
vyshnav <vyshnavravi599@gmail.com>
2024-10-02 10:39:22 +0530

    Refactor resend-mailer.ts to use updated EmailTemplate path and remove console.log statements

--- src/lib/resend-mailer.ts
+++ src/lib/resend-mailer.ts
@@ -1,32 +1,32 @@
-import { EmailTemplate } from "./email-template";
+import { EmailTemplate } from "../../emails/email-template";
 import { ResendEmailOptions, sendEmail as SendEmailType } from "@/types";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function MailUsingResend({ email, name, OTP }: SendEmailType) {
-  console.log(process.env.RESEND_API_KEY);
-  try {
-    const mailOptions: ResendEmailOptions = {
-      from: '"Tedx SJEC" <conceevo@joywincodes.tech>',
-      to: email,
-      subject: "Tedx SJEC - Your OTP for Email Verification",
-      react: EmailTemplate({
-        name: name,
-        OTP: OTP,
-        email: email,
-      }),
-      text: `Hello ${name},\n\nThank you for registering for Tedx 2024.\n\nYour One-Time Password (OTP) for email verification is:\n\n${OTP}\n\nPlease enter this OTP to complete your registration. The OTP is valid for 10 minutes.\n\nThank you!\n\nTedx SJEC Team`,
-    };
+    console.log(process.env.RESEND_API_KEY);
+    try {
+        const mailOptions: ResendEmailOptions = {
+            from: '"Tedx SJEC" <conceevo@joywincodes.tech>',
+            to: email,
+            subject: "Tedx SJEC - Your OTP for Email Verification",
+            react: EmailTemplate({
+                name: name,
+                OTP: OTP,
+                email: email,
+            }),
+            text: `Hello ${name},\n\nThank you for registering for Tedx 2024.\n\nYour One-Time Password (OTP) for email verification is:\n\n${OTP}\n\nPlease enter this OTP to complete your registration. The OTP is valid for 10 minutes.\n\nThank you!\n\nTedx SJEC Team`,
+        };
 
-    const { data, error } = await resend.emails.send(mailOptions);
-    console.log(data, error);
-    if (error) {
-      return { error, status: 500 };
+        const { data, error } = await resend.emails.send(mailOptions);
+        console.log(data, error);
+        if (error) {
+            return { error, status: 500 };
+        }
+
+        return { data, status: 200 };
+    } catch (error) {
+        return { error, status: 500 };
     }
-
-    return { data, status: 200 };
-  } catch (error) {
-    return { error, status: 500 };
-  }
 }
