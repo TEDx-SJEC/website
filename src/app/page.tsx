c2563fce9ebcc09abde3a33e25cb33df88148d6d
pranavvraja <pranavvraja@gmail.com>
2024-10-04 15:06:04 +0530

    responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,16 @@ import ParallaxFooter from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <div className="z-20 relative bg-black ">
+      <div className="z-20 relative bg-black overflow-x-clip">
         <div className="flex flex-col items-center justify-center h-screen p-4"></div>
         <div className="z-10 flex min-h-64 items-center justify-center rounded-lg ">
           <TextReveal text="TEDxSJEC is a platform that brings together curious, creative, and progressive thinkers from St. Joseph Engineering College." />
         </div>
-        <div className="relative p-32 backdrop-invert flex flex-col items-center">
-          <h1 className="text-black text-center text-5xl font-bold leading-tight">
+        <div className="relative h-screen md:p-32 p-8 py-24 backdrop-invert flex flex-col justify-center items-center">
+          <h1 className="text-black text-center md:text-5xl text-2xl w-full font-bold leading-tight">
             Check out the <br /> Previous Editions
           </h1>
-          <div className="mt-24 w-2/3">
+          <div className="mt-24 md:w-2/3 w-full">
             <HeroVideoDialog
               className="block"
               animationStyle="from-center"
