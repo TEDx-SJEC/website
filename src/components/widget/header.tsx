d23c9d15defe31acc30bf510c152db9f3ce41ca7
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-10-05 12:30:26 +0530

    Refactor social links component and use it in header menu

--- src/components/widget/header.tsx
+++ src/components/widget/header.tsx
@@ -3,15 +3,12 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import Image from "next/image";
 import { useRef } from "react";
-import { FiInstagram } from "react-icons/fi";
-import { FaLinkedinIn } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
-import { FaLocationArrow } from "react-icons/fa";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { tedxsjecAssetsPrefix } from "@/lib/utils";
 import React from "react";
 import Link from "next/link";
 import NavItem from "./nav-items";
+import SocialLinks from "../common/social-links";
 
 const Nav = () => {
   gsap.registerPlugin(ScrollTrigger);
@@ -222,43 +219,7 @@ const Nav = () => {
               </h1>
             </div>
           </div>
-
-          <div className="header-nav-footer absolute z-10 bottom-2 md:bottom-3 lg:bottom-10  w-full">
-            <ul className="social-links  cursor-pointer list-none flex items-center w-full">
-              <li className="text-[16px] md:text-[18px] text-black   hover:text-[#EB0028]">
-                <a>
-                  <FiInstagram
-                    size={36}
-                    className="hover:-translate-y-4 transition duration-300"
-                  />
-                </a>
-              </li>
-              <li className="ml-[16px] text-[16px] md:text-[18px] text-black  hover:text-[#EB0028]">
-                <a>
-                  <FaLinkedinIn
-                    size={36}
-                    className="hover:-translate-y-4 transition duration-300"
-                  />
-                </a>
-              </li>
-              <li className="ml-[16px] text-[16px] md:text-[18px] text-black  hover:text-[#EB0028]">
-                <a>
-                  <IoMail
-                    size={36}
-                    className="hover:-translate-y-4 transition duration-300"
-                  />
-                </a>
-              </li>
-              <li className="ml-[16px] text-[16px] md:text-[18px] text-black   hover:text-[#EB0028]">
-                <a className="">
-                  <FaLocationArrow
-                    size={36}
-                    className="hover:-translate-y-4 transition duration-300"
-                  />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <SocialLinks />
         </div>
       </section>
     </>
