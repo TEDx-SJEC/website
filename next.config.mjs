c3a89f0013866a6fea47b1d9df1260b2d15c1e18
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-10-07 21:44:05 +0530

    remvoed default props from navitem

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-        images: {
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'tedx-sjec.github.io',
-              port: '',
-              pathname: '/**',
-            },
-          ],
-        },
-      
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tedx-sjec.github.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "startup-template-sage.vercel.app",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
