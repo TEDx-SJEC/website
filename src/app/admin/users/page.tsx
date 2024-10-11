058cb492332443a52670760df120093c2837c0fb
vyshnav <vyshnavravi599@gmail.com>
2024-10-11 14:25:58 +0530

    feat : add feild to show the number of users

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -3,32 +3,37 @@ import prisma from "@/server/db";
 import React from "react";
 
 export default async function Users() {
-  let initialUserData = await prisma.user.findMany({
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      role: true,
-      image: true,
-    },
-    take: 10,
-  });
-  if (initialUserData === null) {
-    initialUserData = [
-      {
-        id: "1",
-        name: "Test name",
-        email: "testEmail@gmail.com",
-        role: "PARTICIPANT",
-        image: "https://i.pravatar.cc/300?img=1",
-      },
-    ];
-  }
-  return (
-    <>
-      <div className="pt-20 flex min-h-screen w-full flex-col bg-background">
-        <UsersList initialUsers={initialUserData} initialPage={1} />
-      </div>
-    </>
-  );
+    let initialUserData = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            image: true,
+        },
+        take: 10,
+    });
+    const totalNumberOfUsers = await prisma.user.count();
+    if (initialUserData === null) {
+        initialUserData = [
+            {
+                id: "1",
+                name: "Test name",
+                email: "testEmail@gmail.com",
+                role: "PARTICIPANT",
+                image: "https://i.pravatar.cc/300?img=1",
+            },
+        ];
+    }
+    return (
+        <>
+            <div className="pt-20 flex min-h-screen w-full flex-col bg-background">
+                <UsersList
+                    initialUsers={initialUserData}
+                    initialPage={1}
+                    totalNumberOfUsers={totalNumberOfUsers}
+                />
+            </div>
+        </>
+    );
 }
