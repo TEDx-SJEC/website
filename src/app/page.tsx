e46858e277313aa4a907f2038386b3a096ca7015
pranavvraja <pranavvraja@gmail.com>
2024-10-13 01:06:05 +0530

    update: new team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
 import React from "react";
-import HorizontalScroll from "@/components/common/HorizontalScrollCarousel";
-import TextReveal from "@/components/ui/text-reveal";
 import ParallaxFooter from "@/components/common/Footer";
 import StackedCards from "@/components/stacking-cards/stacked";
 import { PreviousEdition } from "@/components/common/Container-Scroll";
 import About from "@/components/common/About";
+import Team from "@/components/common/Team-Section";
 
 export default function Home() {
   return (
     <>
-      <div className="z-20 relative bg-black overflow-x-clip">
+      <div className="z-20 relative bg-blackTheme overflow-x-clip">
         <div
-          className="flex flex-col items-center justify-center h-screen p-4  bg-gradient-to-tr from-red-600 via-black to-black  "
+          className="flex flex-col items-center justify-center h-screen w-full p-4  bg-gradient-to-tr from-red-600 via-black to-black  "
           id="hero"
         ></div>
         <div className="h-screen" id="about">
@@ -22,11 +21,14 @@ export default function Home() {
         <div>
           <StackedCards />
         </div>
-        <div className=" bg-gradient-to-b bg-black from-black via-red-600  to-white to-95%">
+        <div className=" bg-gradient-to-b bg-blackTheme from-blackTheme via-redTheme  to-whiteTheme to-95%">
           <PreviousEdition />
         </div>
-        <div className="backdrop-invert mb-[100vh]">
-          <HorizontalScroll />
+        <div className="bg-whiteTheme mb-[100vh] min-h-screen h-fit">
+          <h1 className="md:text-8xl text-4xl text-center font-black text-blackTheme px-10">
+            The Team
+          </h1>
+          <Team />
         </div>
       </div>
       <ParallaxFooter />
