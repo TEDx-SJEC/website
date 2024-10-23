67f99bafe4d3a905985ed21fd3a8d27fd17175ea
vyshnav <vyshnavravi599@gmail.com>
2024-10-23 20:37:56 +0530

    add cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,37 @@ import StackedCards from "@/components/stacking-cards/stacked";
 import { PreviousEdition } from "@/components/common/Container-Scroll";
 import About from "@/components/common/About";
 import Team from "@/components/common/Team-Section";
+import { CtaSection } from "@/components/common/hero-section";
 
 export default function Home() {
-  return (
-    <>
-      <div className="z-20 relative bg-blackTheme overflow-x-clip">
-        <div
-          className="flex flex-col items-center justify-center h-screen w-full p-4  bg-gradient-to-tr from-red-600 via-black to-black  "
-          id="hero"
-        ></div>
-        <div className="h-screen" id="about">
-          <About />
-        </div>
-        <div>
-          <StackedCards />
-        </div>
-        {/* <div className=" bg-gradient-to-b bg-blackTheme from-blackTheme via-redTheme  to-blackTheme to-95%">
+    return (
+        <>
+            <div className="z-20 relative bg-blackTheme overflow-x-clip">
+                <div
+                    className="flex flex-col items-center justify-center h-screen w-full p-4  bg-gradient-to-tr from-red-600 via-black to-black  "
+                    id="hero"
+                ></div>
+                <div className="h-screen" id="about">
+                    <About />
+                </div>
+
+                <div>
+                    <StackedCards />
+                </div>
+                {/* <div className=" bg-gradient-to-b bg-blackTheme from-blackTheme via-redTheme  to-blackTheme to-95%">
           <PreviousEdition />
         </div> */}
-        <div className="bg-blackTheme mb-[100vh] h-fit ">
-          <h1 className="md:text-8xl text-4xl text-center font-black text-whiteTheme px-10">
-            The Team
-          </h1>
-          <Team />
-        </div>
-      </div>
-      <ParallaxFooter />
-    </>
-  );
+                <div className="bg-blackTheme mb-[100vh] h-fit ">
+                    <h1 className="md:text-8xl text-4xl text-center font-black text-whiteTheme px-10">
+                        The Team
+                    </h1>
+                    <Team />
+                </div>
+                <div>
+                    <CtaSection />
+                </div>
+            </div>
+            <ParallaxFooter />
+        </>
+    );
 }
