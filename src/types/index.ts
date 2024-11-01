ff9f4ffe94fd6a615eb075a1b9bca282475924dc
vyshnav <vyshnavravi599@gmail.com>
2024-11-01 12:03:42 +0530

    Refactor types/index.ts to update interfaces for email options and form data

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,23 +3,37 @@ import { ReactNode } from "react";
 export type UserRoleType = "ADMIN" | "PARTICIPANT";
 
 export interface EmailOptions {
-  from: string;
-  to: string;
-  subject: string;
-  html: string;
-  text: string;
+    from: string;
+    to: string;
+    subject: string;
+    html: string;
+    text: string;
 }
 
 export interface sendEmail {
-  email: string;
-  name: string;
-  OTP: string;
+    email: string;
+    name: string;
+    OTP: string;
 }
 
 export interface ResendEmailOptions {
-  from: string;
-  to: string;
-  subject: string;
-  react: ReactNode;
-  text: string;
+    from: string;
+    to: string;
+    subject: string;
+    react: ReactNode;
+    text: string;
+}
+
+export interface FormDataInterface {
+    designation: "student" | "faculty" | "employee";
+    foodPreference: "veg" | "non-veg";
+    name: string;
+    email: string;
+    phone: string;
+    entityName?: string;
+    photo: string;
+    idCard?: string;
+    usn?: string;
+    amount: string;
+    couponCode?: string;
 }
