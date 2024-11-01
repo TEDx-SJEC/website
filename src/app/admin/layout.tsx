a6bceaf52d91866b909fb0e0ccf770239da66373
vyshnav <vyshnavravi599@gmail.com>
2024-11-01 12:02:45 +0530

    Refactor RootLayout component to handle unauthorized and forbidden access

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -9,30 +10,32 @@ import { useSession } from "next-auth/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  const { data: session } = useSession({
-    required: true,
-  });
-  if (!session) {
-    return <div>Unauthorized</div>;
-  }
+    const { data: session } = useSession({
+        required: true,
+    });
 
-  if (session.user.role !== "ADMIN") {
-    return <div>Forbidden</div>;
-  }
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          <div className="flex">
-            <AdminNavbar />
-            {children}
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+    if (!session) {
+        return <div className="w-screen h-screen flex justify-center items-center">Unauthorized</div>;
+    }
+
+    if (session.user.role !== "ADMIN") {
+        return <div className="w-screen h-screen flex justify-center items-center">Forbidden</div>;
+    }
+
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Providers>
+                    <div className="flex h-screen overflow-hidden">
+                        <AdminNavbar />
+                        <main className="flex-1 overflow-auto bg-indigo-50">{children}</main>
+                    </div>
+                </Providers>
+            </body>
+        </html>
+    );
 }
