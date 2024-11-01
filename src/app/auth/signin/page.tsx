919f550c2e09bfccfcf31499207dbfd199f4d7d6
vyshnav <vyshnavravi599@gmail.com>
2024-11-01 15:14:54 +0530

    Add AuthPage component for signing in

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import Signin from "./signin-page";
+
+export default function AuthPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Signin />
+        </Suspense>
+    );
+}
