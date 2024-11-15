50b9da8ec6ec51331a7f94a8d2aeca3ec643a6b4
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-15 21:46:58 +0530

    renamed container scroll into kebab case

---
+++ src/components/common/container-scroll.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "../ui/container-scroll-animation";
+
+export function PreviousEdition() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="md:text-8xl text-3xl font-semibold text-whiteTheme">
+              Check out the Previous Edition
+            </h1>
+          </>
+        }
+      >
+        {/* <video width="100%" height="100%" controls preload="none" poster="">
+          <source src="/path/to/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video> */}
+        <iframe
+          width="100%"
+          height="450"
+          src="https://www.youtube.com/embed/NCWwTsAjiys?si=xBkmPtQEYQl_tEIX"
+        ></iframe>
+      </ContainerScroll>
+    </div>
+  );
+}
