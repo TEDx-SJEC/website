0adb84b191b8b7592dad49984fc78fc42d5851c2
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-22 21:30:00 +0530

    refactor: moved duplicate speakers vairable into constant folder

--- src/components/common/gridsplash.tsx
+++ src/components/common/gridsplash.tsx
@@ -4,64 +4,7 @@ import { AnimatePresence, motion, useInView } from "framer-motion";
 import Image from "next/image";
 import { X } from "lucide-react";
 import { tedxsjecAssetsPrefix } from "@/lib/utils";
-const speakers = [
-  {
-    id: 1,
-    name: "Karen Kshiti Suvarna",
-    profession: "Film Director",
-    description:
-      "Karen Kshiti Suvarna's debut short film, Hide & Seek, has made waves in the film industry, winning the Best Debut Director (Female) at the Dadasaheb Phalke Achievers Awards 2024. The film has also been showcased at the prestigious Cannes Film Festival. It has also earned accolades across 12 other international festivals and received 15 nominations.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image7.jpg`,
-  },
-  {
-    id: 2,
-    name: "Suma R Nayak",
-    profession: "Advocate and Animal Welfare Activist",
-    description:
-      "Meet Mrs Suma R Nayak, an advocate by profession and animal & environment welfare activist by choice, who believes every creation of God has a right to live a life devoid of pain, suffering and live in dignity. She is the recipient of several awards for her services in the field of environment protection and animal welfare.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image1.jpg`,
-  },
-  {
-    id: 3,
-    name: "Badekkila Pradeep",
-    profession: "Actor | Voice Artist | Anchor",
-    description:
-      "Badekkila Pradeep is a versatile actor, model, writer, and distinguished voice artist from Karnataka. Beginning as a reporter in 2006, Pradeep found his passion in voice-over, transforming Kannada TV narration with his unique style. He's voiced popular shows like Bigg Boss Kannada, Bangalore metro announcements, and numerous campaigns across languages.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image2.jpg`,
-  },
-  {
-    id: 4,
-    name: "Namitha Marimuthu",
-    profession: "International Model, Actress",
-    description:
-      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image3.jpg `,
-  },
-  {
-    id: 5,
-    name: "Namitha Marimuthu",
-    profession: "International Model, Actress",
-    description:
-      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image4.jpg `,
-  },
-  {
-    id: 6,
-    name: "Namitha Marimuthu",
-    profession: "International Model, Actress",
-    description:
-      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image5.jpg `,
-  },
-  {
-    id: 7,
-    name: "Namitha Marimuthu",
-    profession: "International Model, Actress",
-    description:
-      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
-    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image6.jpg `,
-  },
-];
+import { speakers } from "@/constants";
 
 function UnsplashGrid() {
   const [selected, setSelected] = useState(null);
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,65 @@
-// export const basePrice = 980.4;
-export const basePrice = 10;
+import { tedxsjecAssetsPrefix } from "@/lib/utils";
+import { Speaker } from "@/types";
+
+export const basePrice = 1000;
 export const initialdiscount = 0;
-export const sjecStudentPrice = 7.5;
-export const sjecFacultyPrice = 8.0;
+export const sjecStudentPrice = 750;
+export const sjecFacultyPrice = 800;
+export const speakers: Speaker[] = [
+  {
+    id: 1,
+    name: "Karen Kshiti Suvarna",
+    profession: "Film Director",
+    description:
+      "Karen Kshiti Suvarna's debut short film, Hide & Seek, has made waves in the film industry, winning the Best Debut Director (Female) at the Dadasaheb Phalke Achievers Awards 2024. The film has also been showcased at the prestigious Cannes Film Festival. It has also earned accolades across 12 other international festivals and received 15 nominations.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image7.jpg`,
+  },
+  {
+    id: 2,
+    name: "Suma R Nayak",
+    profession: "Advocate and Animal Welfare Activist",
+    description:
+      "Meet Mrs Suma R Nayak, an advocate by profession and animal & environment welfare activist by choice, who believes every creation of God has a right to live a life devoid of pain, suffering and live in dignity. She is the recipient of several awards for her services in the field of environment protection and animal welfare.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image1.jpg`,
+  },
+  {
+    id: 3,
+    name: "Badekkila Pradeep",
+    profession: "Actor | Voice Artist | Anchor",
+    description:
+      "Badekkila Pradeep is a versatile actor, model, writer, and distinguished voice artist from Karnataka. Beginning as a reporter in 2006, Pradeep found his passion in voice-over, transforming Kannada TV narration with his unique style. He's voiced popular shows like Bigg Boss Kannada, Bangalore metro announcements, and numerous campaigns across languages.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image2.jpg`,
+  },
+  {
+    id: 4,
+    name: "Namitha Marimuthu",
+    profession: "International Model, Actress",
+    description:
+      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image3.jpg `,
+  },
+  {
+    id: 5,
+    name: "Namitha Marimuthu",
+    profession: "International Model, Actress",
+    description:
+      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image4.jpg `,
+  },
+  {
+    id: 6,
+    name: "Namitha Marimuthu",
+    profession: "International Model, Actress",
+    description:
+      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image5.jpg `,
+  },
+  {
+    id: 7,
+    name: "Namitha Marimuthu",
+    profession: "International Model, Actress",
+    description:
+      "Namitha Marimuthu is an international model, actress, and social activist who has made history as the first transgender woman to reach the finals of Miss Universe India in 2024. She is the CEO and founder of Miss Queen India and the owner of Alfeem India, both of which promote inclusivity and empowerment.",
+    img: `${tedxsjecAssetsPrefix}/previous_Edition_photo's/image6.jpg `,
+  },
+];
