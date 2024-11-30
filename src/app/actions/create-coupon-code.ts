c3d8b8c4773d7fda6110cd2b2eba2d9d1a6c42c2
vyshnav <vyshnavravi599@gmail.com>
2024-11-30 16:26:43 +0530

    refactor: Improve error handling and add multiple coupon code creation in saveCoupon function

--- src/app/actions/create-coupon-code.ts
+++ src/app/actions/create-coupon-code.ts
@@ -1,33 +1,46 @@
 "use server";
+import { generateCouponCode } from "@/lib/helper";
 import prisma from "@/server/db";
 import { couponSchema } from "@/utils/zod-schemas";
 
 export const saveCoupon = async (
-  coupon: string,
-  createdById: string,
-  discount: number = 20
+    coupon: string,
+    createdById: string,
+    discount: number = 20,
+    numberOfCoupons: number = 1
 ) => {
-  try {
-    const validatCoupon = couponSchema.parse({ coupon, createdById, discount });
-    const couponExists = await prisma.referral.findFirst({
-      where: {
-        code: validatCoupon.coupon,
-      },
-    });
-    if (couponExists) {
-      throw new Error("Coupon code already exists");
+    try {
+        const validatCoupon = couponSchema.parse({ coupon, createdById, discount });
+
+        // Check if the coupon already exists
+        const couponExists = await prisma.referral.findFirst({
+            where: { code: validatCoupon.coupon },
+        });
+        if (couponExists) {
+            throw new Error("Coupon code already exists");
+        }
+
+        // Create coupons
+        const createCoupon = async (code: string) => {
+            return prisma.referral.create({
+                data: {
+                    code,
+                    isUsed: false,
+                    createdById: validatCoupon.createdById,
+                    discountPercentage: validatCoupon.discount.toString(),
+                },
+            });
+        };
+
+        const couponCodes =
+            numberOfCoupons === 1
+                ? [validatCoupon.coupon]
+                : Array.from({ length: numberOfCoupons }, () => generateCouponCode(10));
+
+        const responses = await Promise.all(couponCodes.map(createCoupon));
+        return responses;
+    } catch (error) {
+        console.error("Error creating coupon:", error);
+        throw new Error("Failed to create coupon. Please try again later.");
     }
-    const resp = await prisma.referral.create({
-      data: {
-        code: validatCoupon.coupon,
-        isUsed: false,
-        createdById: validatCoupon.createdById,
-        discountPercentage: validatCoupon.discount.toString(),
-      },
-    });
-    return resp;
-  } catch (error) {
-    console.error("Error creating coupon:", error);
-    throw new Error("Failed to create coupon. Please try again later.");
-  }
 };
