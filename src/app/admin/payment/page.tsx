1d7680939684402dbae7903a1b0b5b41392c3d05
vyshnav <vyshnavravi599@gmail.com>
2024-12-02 17:32:08 +0530

    Refactor payment page to use PaymentCards component

--- src/app/admin/payment/page.tsx
+++ src/app/admin/payment/page.tsx
@@ -1,12 +1,12 @@
-import { SearchableInfiniteScrollTable } from "@/components/common/searchable-infinite-scroll-table";
+import { PaymentCards } from "@/components/common/searchable-infinite-scroll-table";
 import React from "react";
 
 export default async function Payments() {
-  return (
-    <>
-      <div className="p-8 flex min-h-screen w-full flex-col bg-background">       
-        <SearchableInfiniteScrollTable />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="p-8 flex min-h-screen w-full flex-col bg-background">
+                <PaymentCards />
+            </div>
+        </>
+    );
 }
