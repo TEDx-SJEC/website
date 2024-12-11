80922b57c4b1fd2080c997a9e1d9c8ac011ce7ca
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-12-11 11:17:40 +0530

    Refactor changeEmail functionality to include form retrieval and update methods with admin authorization

--- src/app/actions/change-email.ts
+++ src/app/actions/change-email.ts
@@ -3,21 +3,38 @@
 import prisma from "@/server/db";
 import { getServerSession } from "next-auth";
 
-async function changeEmail(email: string) {
+export async function getFormDetailsByEmail(email: string) {
   const session = await getServerSession();
   if (!session || session.user.role !== "ADMIN") {
     return { message: "Unauthorized", isOk: false };
   }
   try {
-    await prisma.user.update({
-      where: { email },
-      data: { email },
+    let forms = await prisma.form.findMany({
+      where: {
+        email: email,
+      },
     });
-    return { message: "Email updated successfully", isOk: true };
+    return forms;
   } catch (error) {
     console.error("Error updating user email:", error);
     return { message: "Error updating user email", isOk: false };
   }
 }
 
-export default changeEmail;
+export async function changeEmail(formId: string, email: string) {
+  const session = await getServerSession();
+  if (!session || session.user.role !== "ADMIN") {
+    return { message: "Unauthorized", isOk: false };
+  }
+  try {
+    const updatedForm = await prisma.form.update({
+      where: { id: formId },
+      data: { email },
+    });
+    return updatedForm;
+  } catch (error) {
+    console.error("Error updating user email:", error);
+    return { message: "Error updating user email", isOk: false };
+  }
+}
+
